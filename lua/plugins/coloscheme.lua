d24b8a813e33034a3a4600a5a7ba5fec30f20527
Simon Aden <simonadn168@gmail.com>
2026-01-29 00:18:02 +0100

    added github dark as coloschema

--- lua/plugins/coloscheme.lua
+++ lua/plugins/coloscheme.lua
@@ -1,59 +1,14 @@
 local colorscheme = require("lazyvim.plugins.colorscheme")
 return {
-  "catppuccin/nvim",
-  lazy = true,
-  name = "catppuccin",
-  opts = {
-    lsp_styles = {
-      underlines = {
-        errors = { "undercurl" },
-        hints = { "undercurl" },
-        warnings = { "undercurl" },
-        information = { "undercurl" },
-      },
-    },
-    integrations = {
-      aerial = true,
-      alpha = true,
-      cmp = true,
-      dashboard = true,
-      flash = true,
-      fzf = true,
-      grug_far = true,
-      gitsigns = true,
-      headlines = true,
-      illuminate = true,
-      indent_blankline = { enabled = true },
-      leap = true,
-      lsp_trouble = true,
-      mason = true,
-      mini = true,
-      navic = { enabled = true, custom_bg = "lualine" },
-      neotest = true,
-      neotree = true,
-      noice = true,
-      notify = true,
-      snacks = true,
-      telescope = true,
-      treesitter_context = true,
-      which_key = true,
-    },
-  },
-  specs = {
-    {
-      "akinsho/bufferline.nvim",
-      optional = true,
-      opts = function(_, opts)
-        if (vim.g.colors_name or ""):find("catppuccin") then
-          opts.highlights = require("catppuccin.special.bufferline").get_theme()
-        end
-      end,
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+  "projekt0n/github-nvim-theme",
+  name = "github-theme",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require("github-theme").setup({
+      -- ...
+    })
+
+    vim.cmd("colorscheme github_dark")
+  end,
 }
